8599f0f4363e17b2be69234472f0c5f5e3539e4d
Jeba Bashir Mujawar <jebamujawar351@gmail.com>
2025-12-21 09:42:37 +0530

    Updated site

--- script.js
+++ script.js
@@ -15,15 +15,26 @@ document.getElementById("contactForm").addEventListener("submit", function(e) {
     let name = document.getElementById("name").value.trim();
     let email = document.getElementById("email").value.trim();
     let message = document.getElementById("message").value.trim();
+    let phone = document.getElementById("phone").value.trim();
     let msg = document.getElementById("formMessage");
 
-    if (name === "" || email === "" || message === "") {
+    // Phone number pattern (10 digits)
+        const phonePattern = /^[0-9]{10}$/;
+
+
+    if (name === "" || email === "" || phone === "" || message === "") {
         msg.innerText = "All fields are required!";
         msg.style.color = "red";
+        return;
     } else if (!email.includes("@")) {
         msg.innerText = "Please enter a valid email!";
         msg.style.color = "red";
-    } else {
+        return;
+    }  else if (!phonePattern.test(phone)) {
+        msg.innerText = "Please enter a valid 10 digit number";
+        msg.style.color = "red";
+        return;
+    }else {
         msg.innerText = "Message sent successfully!";
         msg.style.color = "green";
     }
